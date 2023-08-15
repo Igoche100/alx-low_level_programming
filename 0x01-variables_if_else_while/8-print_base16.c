0d84f2252a1e96136468388bc202f956236a060b
Igoche 100 <igochemonday@yahoo.com>
2023-08-14 20:09:41 -0700

    variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - All the numbers of base 16 in lowercase, followed by a new line
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int num;
+char letter;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
